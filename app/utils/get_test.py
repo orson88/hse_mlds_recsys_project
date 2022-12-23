320522f7b5114c56754319dc58e29b179fdec9bb
vlad <lector45rus@gmail.com>
2022-12-23 10:54:10 +0300

    test updated

--- app/utils/get_test.py
+++ app/utils/get_test.py
@@ -9,16 +9,22 @@ def get_test(df: pd.DataFrame) -> pd.DataFrame:
     :param df: All DataFrame
     :return: Test DataFrame
     """
-    workdir = Path(__file__)
+    workdir = Path(__file__).parent
 
-    with open(workdir / 'test_index.json') as f:
+    if 'song' not in df.columns:
+        df['artistname'].fillna('', inplace=True)
+        df['trackname'].fillna('', inplace=True)
+
+        df['song'] = df['artistname'] + '__' + df['trackname']
+
+    with open(workdir.parent / 'data' / 'test_index.json') as f:
         test_index = json.load(f)
 
     test = (df.loc[test_index]
             .groupby('user_id')['song'].unique()
             .reset_index())
 
-    test.rename(columns={'song': 'actual'})
+    test.rename(columns={'song': 'actual'}, inplace=True)
 
     assert test.shape[0] == 14560
 
