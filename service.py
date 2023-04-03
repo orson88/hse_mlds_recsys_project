7589dec104555d8ed31286eed747e40e95ef27d6
Andrei Iurkin <9873203@mail.ru>
2023-04-03 12:19:48 +0300

    second FastApi iteration

--- service.py
+++ service.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import pickle
 from scipy.sparse import load_npz
-
+import pandas as pd
 
 app = FastAPI(
     title="RecSys App"
@@ -13,6 +13,7 @@ async def root():
     return "root"
 
 
+popular_songs = pd.read_csv("datasets/popular_songs.csv")
 user_item_matrix = load_npz('app/data/train_user_item_matrix.npz')
 user_item_matrix = user_item_matrix.astype('float').tocsr()
 
@@ -25,23 +26,33 @@ with open('app/data/userids.pkl', 'rb') as f:
 with open('app/data/itemids.pkl', 'rb') as f:
     itemids = pickle.load(f)
 
+# Словари для перевода userid в id и наоборот
 userid_to_id = {user_id: idx for idx, user_id in enumerate(userids)}
 id_to_userid = {val: key for key, val in userid_to_id.items()}
 
+# Словари для перевода itemid в id и наоборот
 item_to_id = {item_id: idx for idx, item_id in enumerate(itemids)}
 id_to_item = {val: key for key, val in item_to_id.items()}
 
 
 @app.get("/predictions")
 async def get_recommendations(user_id: str = '00055176fea33f6e027cd3302289378b', n: int = 5):
-    user_index = userid_to_id[user_id]
-    recs = als.recommend(
-        userid=user_index,
-        user_items=user_item_matrix[user_index],
-        N=n,  # кол-во рекомендаций
-        filter_already_liked_items=False,
-        filter_items=None,
-        recalculate_user=False
-    )
+    if user_id in userid_to_id:  # рекомендации для луществующих пользователей
+        user_index = userid_to_id[user_id]
+        recs = als.recommend(
+            userid=user_index,
+            user_items=user_item_matrix[user_index],
+            N=n,  # кол-во рекомендаций
+            filter_already_liked_items=False,
+            filter_items=None,
+            recalculate_user=False
+        )
 
-    return [id_to_item[rec] for rec in recs[0]]
+        return {"user": "known user",
+                "n_recommendations": n,
+                "recommendation": [id_to_item[rec] for rec in recs[0]]}
+
+    else:  # самые популярные треки для новых пользователей
+        return {"user": "new user",
+                "n_recommendations": n,
+                "recommendation": list(popular_songs.iloc[:n, 1])}
