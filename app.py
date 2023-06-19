addb9b1251c9ef57ac4708728c5d836f0897ced9
orson88 <62896830+orson88@users.noreply.github.com>
2023-06-19 13:53:15 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,135 @@
+import psycopg2
+import pandas as pd
+import scipy.sparse as scpspr
+import pickle
+from fastapi import FastAPI
+import implicit 
+
+class MyConnection:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host = "10.244.2.88",
+            port = 5432,
+            user = 'postgres',
+            password = 'test1',
+            database = 'postgres'
+        )
+    def reinit_conn(self):
+        self.conn = psycopg2.connect(
+            host = "10.244.2.88",
+            port = 5432,
+            user = 'postgres',
+            password = 'test1',
+            database = 'postgres'
+        )
+    def get_query(self, query):
+        self.reinit_conn()
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        df = pd.DataFrame(
+            data,
+            columns=[desc[0] for desc in cursor.description]
+        )
+        cursor.close()
+        self.conn.close()
+        return df
+
+    def post_query(self, query):
+        self.reinit_conn()
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        self.conn.commit()
+        cursor.close()
+
+
+
+
+
+
+
+MC = MyConnection()
+
+
+#### ДОЗА КОСТЫЛЕЙ
+popular_songs = pd.read_csv(f"/var/popular_songs.csv")
+user_item_matrix = scpspr.load_npz('/var/train_user_item_matrix.npz')
+user_item_matrix = user_item_matrix.astype('float').tocsr()
+with open('/var/als_best_params.pkl', 'rb') as f:
+    als = pickle.load(f)
+
+def gen_new_user_id():
+    cnt = MC.get_query(f"""
+    SELECT COUNT(DISTINCT user_id)
+    FROM (
+    SELECT user_id FROM train_table
+    UNION
+    SELECT user_id FROM test_table
+    ) combined_set;
+    """)['count'][0]
+    return cnt+1
+
+def gen_new_item_id():
+    cnt = MC.get_query(f"""
+    SELECT COUNT(DISTINCT song)
+    FROM (
+    SELECT song FROM train_table
+    UNION
+    SELECT song FROM test_table
+    ) combined_set;
+    """)['count'][0]
+    return cnt+1
+
+#### ЗАПРОСЫ К БД
+
+
+userids = MC.get_query("SELECT DISTINCT user_id FROM train_table").values
+userids = [_[0] for _ in userids]
+itemids = MC.get_query("SELECT DISTINCT song FROM train_table").values
+itemids = [_[0] for _ in itemids]
+
+
+userid_to_id = {user_id: idx for idx, user_id in enumerate(userids)}
+id_to_userid = {val: key for key, val in userid_to_id.items()}
+
+item_to_id = {item_id: idx for idx, item_id in enumerate(itemids)}
+id_to_item = {val: key for key, val in item_to_id.items()}
+
+#### АПИ (AKA - Пошла Жара)
+app = FastAPI()
+@app.get("/predictions")
+async def get_recommendations(user_id:str, n:str):
+    
+    n = int(n)
+    if user_id in userid_to_id:  # рекомендации для луществующих пользователей
+        user_index = userid_to_id[user_id]
+        recs = als.recommend(
+            userid=user_index,
+            user_items=user_item_matrix[user_index],
+            N=n,  # кол-во рекомендаций
+            filter_already_liked_items=False,
+            filter_items=None,
+            recalculate_user=False
+        )
+
+        return [id_to_item[rec] for rec in recs[0]]
+
+    else:  # самые популярные треки для новых пользователей
+        return list(popular_songs.iloc[:n, 1])
+
+
+@app.get("/all_users")
+async def get_users(input):
+    if input == 'gigalul':
+        return list(MC.get_query("SELECT DISTINCT user_id FROM train_table")['user_id'].values)
+    else:
+        return "asdf"
+
+@app.post('/register')
+async def reg_new_reaction(input: str):
+    if len(input.split(' --- ')) == 2:
+        if len(input.split('__')) == 2:
+            MC.post_query(f"""
+            INSERT INTO test_table (user_id, song, new_user_id, song_id, connect)
+            VALUES ('{input.split(' --- ')[0]}', '{input.split(' --- ')[1]}', {gen_new_user_id()}, {gen_new_item_id()}, 1);
+            """)
---
+++ frontend.py
@@ -0,0 +1,114 @@
+import streamlit as st
+import requests
+import pickle
+import os
+import json
+import pandas as pd
+from itertools import compress
+
+
+###### st.session_state['new_users'] = []
+### shariki ebanie
+
+def postdude(user:str, song:str):
+    user = user.replace(' ', '%20')
+    song = song.replace(' ', '%20')
+    post_string = f"http://localhost:8000/register?input={user}%20---%20{song}"
+    request.post(post_string)
+
+def onClickFunction():
+    st.session_state.infer = True
+
+def get_user_ids():
+    return requests.get(
+        url="http://localhost:8000/all_users?input=gigalul"
+        ).text\
+            .strip('[""]')\
+            .split('","')
+
+def get_recs(option):
+    result = requests.get(url=f"http://localhost:8000/predictions?user_id={option}&n=5")\
+        .text.split('","')
+    return result
+
+def main():
+
+    if 'sidebar_state' not in st.session_state:
+        st.session_state.sidebar_state = 'expanded'
+
+    st.set_page_config(initial_sidebar_state=st.session_state.sidebar_state)
+
+    if st.session_state.sidebar_state != 'expanded':
+        st.balloons()
+
+    if 'infer' not in st.session_state:
+        st.session_state.click = False
+
+
+    userids = get_user_ids()
+
+    
+
+    ### SIDEBAR
+    sdb = st.sidebar
+    sdb.write('If you cant find your username, please register here!')
+    new_u_username = sdb.text_input('input your name here', key='newuid')
+    new_u_song = sdb.text_input('please, provide us your favourite song!', key='newusong')
+    new_u_author = sdb.text_input('please, name the author of the song', key='newauthor')
+    if_commit = sdb.button("Press this button when you are sure that the format is proper to register yourself!")
+
+    if st.session_state.sidebar_state != 'expanded':
+        sdb.write(f"Thast.write(song)nk you, {new_u_username}! You have already registered!")
+
+    if if_commit:
+        if len(new_u_username) > 0 and len(new_u_song) > 0 and len(new_u_author):
+            sdb.write(f"done! Thank you, {new_u_username}!")
+            st.session_state.sidebar_state = 'collapsed' if st.session_state.sidebar_state == 'expanded' else 'expanded'
+            st.experimental_rerun()
+            st.balloons()
+        else:
+            sdb.write('wrong format')
+    
+    ### MAIN PAGE
+
+    ### TITLES
+    st.header('Welcome to our music recomender system!')
+    st.write('Please, register in the sidebar, if you have not registered yet')
+
+    ### USER ID DROPBOX
+    option = st.selectbox('Enter your user id',
+                          userids)
+    ### BODY
+    cont1 = st.container()
+
+    ### GET INFER MAGIC BUTTON 
+    inferBut = cont1.button('get me some recs', on_click = onClickFunction)
+    try:
+        if st.session_state['infer']:
+            cont2 = st.container()
+            cont2.col1, cont2.col2 = st.columns(2)
+            checks = []
+            songs = []
+            for _ in get_recs(option):
+                checks.append(cont2.col1.checkbox(_.strip('["').strip('"]').replace('__', ' -- ')))
+                songs.append(_)
+            cont2.col2.write("Which of them do u like? Checkmark the ones you like and submit the result so we could take your feedback into account further!")
+                
+            ### PART 3
+            cont3 = st.container()
+
+
+            ### SUBMIT BUTTON
+            ifcl = cont3.button("Submit your reactions so we can improve our service!")
+            if ifcl:
+                for song in list(compress(songs, checks)):
+                    postdude(user=option, song=song)
+
+    except:
+        cont1.write('Hi!')
+
+
+
+
+if __name__ == "__main__":
+    main()
