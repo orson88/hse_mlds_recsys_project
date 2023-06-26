b5f5ab64af9e31fcdd127e72eabef7e4421779e3
orson88 <62896830+orson88@users.noreply.github.com>
2023-06-26 19:47:21 +0300

    Update cluster-init.sh

--- cluster-init.sh
+++ cluster-init.sh
@@ -7,6 +7,15 @@ docker exec minikube docker pull orson88/recsys:frontend
 kubectl apply -f /home/orson88/singlecluster/pod_configs/.
 kubectl apply -f /home/orson88/singlecluster/main_services/back/.
 kubectl apply -f /home/orson88/singlecluster/main_services/front/.
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/prometheus
+helm install grafana grafana/grafana
+kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-ext
+kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-ext
+kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+
 
 echo "waiting for pods for 10sec" 
 sleep 10
